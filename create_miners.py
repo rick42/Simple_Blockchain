9bbc440a48fb2ac8c1e173ca2b4a9fec7a16c6f3
Ricky Valdez <vldzrcky@gmail.com>
2020-05-10 18:37:52 -0400

    added shutdown_miner() to create_miners.py

--- create_miners.py
+++ create_miners.py
@@ -1,9 +1,20 @@
 import os
 import node
 import threading
+import requests
 
 def make_miner(port):
     os.system("python node.py --port {}".format(port))
+
+def shutdown_miner(port):
+    url = 'http://localhost:{}/shutdown'.format(port)
+    try:
+        response = requests.post(url)
+        # if response.status_code == 400 or response.status_code == 500:
+        #     print('Transaction declined, needs resolving')
+        #     return False
+    except requests.exceptions.ConnectionError:
+        pass
 
 if __name__ == '__main__':
     threads = list()
@@ -12,5 +23,12 @@ if __name__ == '__main__':
         x = threading.Thread(target=make_miner, args=(5000+i,))
         threads.append(x)
         x.start()
+
+    input('Press Enter to shutdown node localhost:5000')
+
+    shutdown_miner(5000)
+
+    input('Press Enter again to end script')
+
     
     print('THREADS CREATED!!')
